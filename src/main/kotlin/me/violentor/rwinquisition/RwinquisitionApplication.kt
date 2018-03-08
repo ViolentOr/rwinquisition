3b80e9b19550620d4c01459e17d100a1e8044ee5
Mac <github@violentor.me>
2018-03-08 20:07:08 +0300

    Initial commit

---
+++ src/main/kotlin/me/violentor/rwinquisition/RwinquisitionApplication.kt
@@ -0,0 +1,11 @@
+package me.violentor.rwinquisition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RwinquisitionApplication
+
+fun main(args: Array<String>) {
+    runApplication<RwinquisitionApplication>(*args)
+}
